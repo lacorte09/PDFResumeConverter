6331773a23e9254f40cd13e231dacc3d74896716
lacorte09 <lacorte08cristan@gmail.com>
2022-07-14 17:54:07 +0800

    initial

---
+++ ResumeConverter/ResumeConverter/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ResumeConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.jsonHere1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.readKita1 = new System.Windows.Forms.Button();
+            this.generateKita2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // jsonHere1
+            // 
+            this.jsonHere1.Location = new System.Drawing.Point(97, 60);
+            this.jsonHere1.Multiline = true;
+            this.jsonHere1.Name = "jsonHere1";
+            this.jsonHere1.Size = new System.Drawing.Size(304, 285);
+            this.jsonHere1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // readKita1
+            // 
+            this.readKita1.Location = new System.Drawing.Point(97, 31);
+            this.readKita1.Name = "readKita1";
+            this.readKita1.Size = new System.Drawing.Size(75, 23);
+            this.readKita1.TabIndex = 2;
+            this.readKita1.Text = "button1";
+            this.readKita1.UseVisualStyleBackColor = true;
+            this.readKita1.Click += new System.EventHandler(this.readKita1_Click);
+            // 
+            // generateKita2
+            // 
+            this.generateKita2.Location = new System.Drawing.Point(178, 31);
+            this.generateKita2.Name = "generateKita2";
+            this.generateKita2.Size = new System.Drawing.Size(75, 23);
+            this.generateKita2.TabIndex = 3;
+            this.generateKita2.Text = "generateMe1";
+            this.generateKita2.UseVisualStyleBackColor = true;
+            this.generateKita2.Click += new System.EventHandler(this.kahitAnoMuna2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 502);
+            this.Controls.Add(this.generateKita2);
+            this.Controls.Add(this.readKita1);
+            this.Controls.Add(this.jsonHere1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox jsonHere1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button readKita1;
+        private System.Windows.Forms.Button generateKita2;
+    }
+}
+
---
+++ ResumeConverter/ResumeConverter/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace ResumeConverter
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void readKita1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string jsonReadFile;
+                var readMe = new StreamReader(@"C:\Users\valen\O-O-P\ResumeConverter\Resume.json");              
+                {
+                    jsonReadFile = readMe.ReadToEnd();
+                    jsonHere1.Text = jsonReadFile;
+
+                }
+                
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void kahitAnoMuna2_Click(object sender, EventArgs e)
+        {
+            Document docx = new Document();
+            PdfWriter.GetInstance(docx, new FileStream(@"C:\Users\valen\O-O-P\ResumeConverter\Resume.pdf", FileMode.Create));
+            docx.Open();
+            Paragraph par1 = new Paragraph(jsonHere1.Text);
+            docx.Add(par1);
+            docx.Close();
+            
+        }
+    }
+}
